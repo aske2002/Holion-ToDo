3fe8efc0f3c1b61b378f8db782742e97c3726dee
aske2002 <askevkoed@gmail.com>
2023-01-15 13:01:31 +0100

    Updating swagger config

--- todo-backend/todo-backend/Program.cs
+++ todo-backend/todo-backend/Program.cs
@@ -62,10 +62,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
