ada71427c5c3cfd3e98bbab3910b237ebc01de22
aske2002 <askevkoed@gmail.com>
2023-01-15 15:26:55 +0100

    Setup action

--- todo-backend/todo-backend/Program.cs
+++ todo-backend/todo-backend/Program.cs
@@ -63,12 +63,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseSwagger();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI();
-}
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
